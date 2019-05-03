f0a8e7213a32d6f5c8f71a4449062a8633627789
Duoqi Zheng <zhedy005@mymail.unisa.edu.au>
2019-05-04 00:18:43 +0930

    Add Filter

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.ViewModels;
 
 namespace MyShop.WebUI.Controllers
 {
@@ -18,10 +19,22 @@ namespace MyShop.WebUI.Controllers
             context = productContext;
             productCategories = productCategoryContext;
         }
-        public ActionResult Index()
+        public ActionResult Index(string category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> productCategory = productCategories.Collection().ToList();
+            if(category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == category).ToList();
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Product = products;
+            model.ProductCategories = productCategory;
+            return View(model);
         }
 
         public ActionResult About()
